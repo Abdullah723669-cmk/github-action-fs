51f0107a47efe0cd9442ad745c59ad2c2b729026
Abdullah723669-cmk <mamun.renata52@gmail.com>
2025-07-14 11:44:13 -0400

    added test

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
+const http = require('http');
+const app = require('./app');
 
-// app.js
+const server = http.createServer(app);
 
-const express = require('express');
-const app = express();
-const port = 3000;
-
-// Serve a simple HTML page
-app.get('/', (req, res) => {
-  res.send(`
-    <html>
-      <head>
-        <title>Mamun's Demo App</title>
-      </head>
-      <body style="font-family: sans-serif; text-align: center;">
-        <h1>Hello from Express.js! --mamun</h1>
-        <p>This is a demo Node.js app with HTML response.</p>
-      </body>
-    </html>
-  `);
-});
-
-app.listen(port, () => {
-  console.log(`App running at http://localhost:${port}`);
+server.listen(3000, ()=> {
+  console.log("server is running on port 3000")
 });
---
+++ tests/app.test.js
@@ -0,0 +1,20 @@
+const request = require('supertest');
+
+const app = require('../app');
+
+describe('GET /', function( ){
+    it('responds with text "Hello Mamun" ', async function ( ) {
+        const  chai = await import('chai');
+        const expect = chai.expect;
+
+
+        await request(app)
+        .get('/')
+        .expect(200)
+        .then((res)=>{
+            expect(res.text).to.equal('Hello Mamun');
+        });
+
+        
+    });
+});
